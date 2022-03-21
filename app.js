bdb73088adf570d11910e07fc4ba96c149cbb098
Сорокина Полина Евгеньевна <PESorokina@sberbank.ru>
2022-03-21 13:39:08 +0300

    fix: failed build

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ router.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + 'dist/index.hbs'));
 });
 
-//add the router
 app.listen(process.env.port || 3000);
 
 console.log('Running at Port 3000');
