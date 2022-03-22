5bb2d7d3c64eb3e152eec2f35885b30b70d8ea67
Сорокина Полина Евгеньевна <PESorokina@sberbank.ru>
2022-03-22 16:36:56 +0300

    fix: failed build 3.0

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+
+const app = express();
 const router = express.Router();
 
 app.use(express.static(path.join(__dirname, 'dist')));
